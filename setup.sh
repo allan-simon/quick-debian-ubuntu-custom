c592833974c413dadba783b84c5389869782caa5
Allan SIMON <allan.simon@supinfo.com>
2012-10-09 21:56:39 +0800

    added some missing tools (gdb, unp etc.), and started to add more comments so that my students know why I'm installing this or that

--- setup.sh
+++ setup.sh
@@ -8,19 +8,27 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-
+# maybe add the -y to apt-get install only if this script is invocated with the -y flag
 install_software() {
 
     # first install some tools
-    apt-get install vim zsh htop most sqlite3
+    apt-get -y install vim      #best editor ev4r (troll spotted)
+    apt-get -y install zsh      #got much more advanced features than bash
+    apt-get -y install htop     #a color and improved version of top
+    apt-get -y install most     #a modern version of less and more
+    apt-get -y install unp      #tool to extract any archive
+    apt-get -y install bzip2    #permit to extra bz2 archive (unp use it)
+    apt-get -y install sqlite3  #a simple to manipulate database
+    apt-get -y install locate   #tools to search files
 
     # then some stuff related to programming
-    apt-get install gcc gcc-4.7 clang automake autoconf \
-                    cmake colordiff colormake python3 cppcheck
+    apt-get -y install gcc gcc-4.7 clang automake autoconf \
+                    cmake colordiff colormake python3 cppcheck \
+                    gdb cgdb
                     
 
     # some stuff related to versionning
-    apt-get install subversion git
+    apt-get -y install subversion git
     
 }
 
